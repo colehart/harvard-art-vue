f385c3ddcbdb16c1ee3c27bd9014a351006b75e6
Cole Hart <cole@colehart.com>
2019-01-10 08:49:16 -0700

    Begin api call logic

--- src/components/GalleriesContainer.vue
+++ src/components/GalleriesContainer.vue
@@ -29,28 +29,53 @@
 </template>
 
 <script>
-export default {
-  name: 'GalleriesContainer',
-  props: {
-    msg: String
+  import GalleryInfo from '../services/api/GalleryInfo'
+
+  export default {
+    name: 'GalleriesContainer',
+    props: {
+      msg: String
+    },
+    data () {
+      return {
+        galleries: [],
+        loading: true,
+        errored: false,
+      }
+    },
+    methods: {
+      getGalleries () {
+        GalleryInfo.getGalleries()
+          .then(apiGalleries => {
+            this.galleries = apiGalleries
+          })
+          .catch(error => {
+            console.log(error)
+            this.errored = true
+          })
+          .finally(() => this.loading = false)
+      }
+    },
+    mounted () {
+      this.getGalleries()
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
 </style>
---
+++ src/services/api/GalleryInfo.js
@@ -0,0 +1,15 @@
+import axios from '../config/axios';
+
+export default {
+  getGalleries () {
+    return axios.get('/gallery', {params: {
+      apiKey: '91cd83a0-12bb-11e9-90d1-473127181d8c',
+      size: 100
+    }})
+    .then(response => {
+      return response.data.records.filter(record => {
+        return record.theme && record.labeltext
+      })
+    })
+  }
+}
